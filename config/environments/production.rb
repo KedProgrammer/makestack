902d8346fd1de65262638e248d8028db624e51fb
Daniel Tobon <shiriux1@gmail.com>
2017-07-04 22:50:52 -0500

    account version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<fast-wave-52889.herokuapp.com'
+  host = 'fast-wave-52889.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
